510b52af72592edf689bbc7ad89f03fb24f236f0
Noureddine DRIOUECH <113625766+NoureddineDRIOUECH@users.noreply.github.com>
2023-09-16 20:40:03 +0100

    Update Encryption Program.py

--- Encryption Program.py
+++ Encryption Program.py
@@ -5,13 +5,13 @@ chars = list(chars)
 key = chars.copy()
 random.shuffle(key)
 encryptedmessage = ""
-textuser = input("Enter your text to enrypt : ")
+textuser = input("Enter your text to encrypt : ")
 for i in textuser:
     index = chars.index(i)
     encryptedmessage += key[index]
 print(f"The encyption of your message is : {encryptedmessage}")
 encryptedmessage = ""
-textuser = input("Enter your text to derypt : ")
+textuser = input("Enter your text to decrypt : ")
 for i in textuser:
     index = key.index(i)
     encryptedmessage += chars[index]
